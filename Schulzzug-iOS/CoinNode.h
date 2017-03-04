57da4e6c9978568b3576d3de77162fa94954e527
Niklas Riekenbrauck <niklasriekenbrauck@br00klyn.local>
2017-03-04 22:07:27 +0100

    Add CoinNode

---
+++ Schulzzug-iOS/CoinNode.h
@@ -0,0 +1,13 @@
+//
+//  CoinNode.h
+//  Schulzzug-iOS
+//
+//  Created by Niklas Riekenbrauck on 04.03.17.
+//  Copyright © 2017 Frederik Riedel. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface CoinNode : SKSpriteNode
+
+@end
