702c56385bf1f2d1fa631b85175b43166f82cb16
Niklas Riekenbrauck <niklasriekenbrauck@br00klyn.local>
2017-03-04 19:05:23 +0100

    Add basic level and game object2

---
+++ Schulzzug-iOS/Game.swift
@@ -0,0 +1,39 @@
+//
+//  Game.swift
+//  Schulzzug-iOS
+//
+//  Created by Niklas Riekenbrauck on 04.03.17.
+//  Copyright © 2017 Frederik Riedel. All rights reserved.
+//
+
+import Foundation
+
+
+@objc protocol GameDelegate {
+    func didStart(game: Game)
+    func didStop(game: Game)
+    func didFinish(game: Game)
+}
+
+
+@objc class Game: NSObject {
+    fileprivate var score: Int = 0
+    fileprivate var schulzCoins: Int = 0
+    
+    fileprivate let gameSpeed: Int = 3
+    fileprivate let updateInterval: Float = 0.1
+    
+    weak var gameDelegate: GameDelegate?
+    
+    func start() {
+        gameDelegate?.didStart(game: self)
+    }
+    
+    func stop() {
+        gameDelegate?.didStop(game: self)
+    }
+    
+    func blockSpawning() {
+        
+    }
+}
