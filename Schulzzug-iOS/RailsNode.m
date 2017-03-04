a7d3ba8004614ccba8dfdf7f661cf44b4ed133c7
Frederik Riedel <frederik.riedel@frogg.io>
2017-03-05 00:35:22 +0100

    added right, center, left rail

--- Schulzzug-iOS/RailsNode.m
+++ Schulzzug-iOS/RailsNode.m
@@ -15,9 +15,8 @@
     
     if(self) {
         
-        NSArray <SKTexture*>* railTextures = @[[SKTexture textureWithImageNamed:@"rails_1"],
-                                               [SKTexture textureWithImageNamed:@"rails_2"]];
-        SKTexture* startTexture = [SKTexture textureWithImageNamed:@"rails_1"];
+        
+        SKTexture* startTexture = [SKTexture textureWithImageNamed:@"rails_center_1"];
         
         int numberOfRails = 3;
         
@@ -25,6 +24,22 @@
         
         
         for(int i=0; i<numberOfRails; i++) {
+            
+            NSArray <SKTexture*>* railTextures = @[];
+            
+            if(i==0) {
+                railTextures = @[[SKTexture textureWithImageNamed:@"rails_left_1"],
+                                 [SKTexture textureWithImageNamed:@"rails_left_2"]];
+            } else if(i==1) {
+                railTextures = @[[SKTexture textureWithImageNamed:@"rails_center_1"],
+                                 [SKTexture textureWithImageNamed:@"rails_center_2"]];
+            } else if(i==2) {
+                railTextures = @[[SKTexture textureWithImageNamed:@"rails_right_1"],
+                                 [SKTexture textureWithImageNamed:@"rails_right_2"]];
+            }
+            
+            
+            
             SKSpriteNode* rail = [[SKSpriteNode alloc] initWithTexture:startTexture];
             rail.anchorPoint = CGPointMake(0, 0);
             rail.position = CGPointMake(-self.size.width/2 + ((i+1)*spacing) + (i*startTexture.size.width), -self.size.height/2);
